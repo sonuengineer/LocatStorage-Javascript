c10d344fd613f666fe4122567bddacf0e02f5646
sonuengineer <prajapatisonu50@gmail.com>
2022-08-16 21:08:17 +0530

    user value fronted

--- main.js
+++ main.js
@@ -7,6 +7,15 @@ const user = {
     mail: mail,
 }
 window.localStorage.setItem(" user details",JSON.stringify(user));  
+
+var records = window.localStorage.getItem("user details");
+    var paragraph = document.createElement("p");
+    var infor = document.createTextNode(records);
+    paragraph.appendChild(infor);
+    var element = document.getElementById("retrieve");
+    element.appendChild(paragraph);
+
+
 }
 
 window.onload =function(){ //ensures the page is loaded before functions are executed.
